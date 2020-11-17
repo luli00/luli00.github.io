6ea1cf6462863e8902ea1cf3e11cf289e4a81c27
Luiza Latorre <luizalatorre@hotmail.com>
2020-11-17 17:33:16 -0300

    Teste de script

---
+++ script.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector('button');
+let div = document.querySelector('div');
+
+btn.addEventListener('click', () => {
+    if(div.style.display === 'none'){
+        div.style.display = 'block';
+    }else {
+        div.style.display = 'none';
+    }
+    }
+})
